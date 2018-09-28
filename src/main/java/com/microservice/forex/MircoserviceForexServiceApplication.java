063b091b98860679495f3131c295186f96eba671
ROBIN SINGH <robinsingh0210@gmail.com>
2018-09-28 16:16:14 +0530

    Enable Eureka discovery client

--- src/main/java/com/microservice/forex/MircoserviceForexServiceApplication.java
+++ src/main/java/com/microservice/forex/MircoserviceForexServiceApplication.java
@@ -2,10 +2,12 @@ package com.microservice.forex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableDiscoveryClient
 public class MircoserviceForexServiceApplication {
 
 	public static void main(String[] args) {
